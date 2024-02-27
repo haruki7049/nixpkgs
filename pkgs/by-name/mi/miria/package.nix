592ee0245b554a8d952edb98a382b24a9edba8f8
haruki7049 <tontonkirikiri@gmail.com>
2024-02-27 18:33:44 +0900

    chore: WIP

--- pkgs/by-name/mi/miria/package.nix
+++ pkgs/by-name/mi/miria/package.nix
@@ -1,28 +1,27 @@
 { lib
-, flutter
-, buildFlutterApplication
+, buildDartApplication
 , fetchFromGitHub
 }:
 
-flutter.buildFlutterApplication {
+buildDartApplication rec {
   pname = "miria";
   version = "1.0.16+100";
 
   src = fetchFromGitHub {
     owner = "shiosyakeyakini-info";
     repo = "miria";
-    rev = "1.0.16+100";
-    hash = lib.fakeHash;
+    rev = "v${version}";
+    hash = "sha256-xLQuJn8rbgAxRjveexqpYK72g4z2qHpFqV8rMQZCe54=";
   };
 
-  pubspecLockFile = "/pubspec.yaml";
-
   vendorHash = "";
+  autoPubspecLock = "${src}/pubspec.lock";
 
   meta = with lib; {
     description = "Misskey client application for iOS, Android (and other platforms)";
     homepage = "https://github.com/shiosyakeyakini-info/miria";
     maintainers = [ maintainers.haruki7049 ];
+    platforms = [ "x86_64-linux" ];
     license = licenses.agpl3;
   };
 }
